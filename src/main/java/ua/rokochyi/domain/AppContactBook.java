991a0fd7097708f137cdd7b131d6ceb4f2709ee7
TarasRokochyi <tarrok2005@gmail.com>
2023-06-28 20:14:39 +0300

    added add, list, methods

--- src/main/java/ua/rokochyi/domain/AppContactBook.java
+++ src/main/java/ua/rokochyi/domain/AppContactBook.java
@@ -3,10 +3,13 @@ package ua.rokochyi.domain;
 import ua.rokochyi.domain.data.Contact;
 import ua.rokochyi.domain.data.ContactDataSource;
 import ua.rokochyi.domain.data.Person;
+import ua.rokochyi.domain.data.Number;
 
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,8 +25,9 @@ public class AppContactBook implements ContactBook{
     }
 
     @Override
-    public void addContact() {
-
+    public void addContact(String name, String second_name, LocalDate birthdayDate, String email, List<Number> phoneNumbers) {
+        Person person = new Person(name, second_name, birthdayDate);
+        contacts.add(new Contact(person, email, phoneNumbers));
     }
 
     @Override
@@ -34,6 +38,19 @@ public class AppContactBook implements ContactBook{
     @Override
     public void updateContact() {
 
+    }
+
+    @Override
+    public void listContact() {
+        for (Contact contact: contacts){
+            System.out.println();
+            System.out.println("initials: " + contact.person().name() + " " + contact.person().second_name() + "\n"+
+                               "birthday: " + contact.person().birthday());
+            for (Number number: contact.phoneNumbers()){
+                System.out.println(number.provider()+": "+ number.phoneNumber());
+            }
+        }
+        System.out.println();
     }
 
     @Override
